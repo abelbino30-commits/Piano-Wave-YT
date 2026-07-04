d4461a268829283df463fbe123769b928bb134f3
abelbino30-commits <abelbino30@gmail.com>
2026-07-04 08:28:44 +0530

    Refactor Streamlit app layout and styling

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import streamlit as st
 
 st.set_page_config(page_title="Piano Wave YT", page_icon="🎹", layout="centered")
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #fcfcfc;
+        color: #111111;
+    }
+    h1, h2, h3, p, div, span {
+        color: #111111 !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("🎹 Piano Wave YT")
 st.markdown("### Where music meets tranquility.")
 
@@ -11,15 +26,4 @@ st.video("https://www.youtube.com/watch?v=YOUR_VIDEO_ID_HERE")
 st.markdown("---")
 
 st.write("Love the vibes? Check out the full channel:")
-st.link_button("Go to Piano Wave YouTube Channel", "https://youtube.com/@pianowaveyt1")
-
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #fcfcfc;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.link_button("Go to Piano Wave YT YouTube Channel", "https://youtube.com/@pianowaveyt1")
